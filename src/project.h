abe8046043e3ab2ea530c7e6a0279b59e30b5b4a
tiannian <dtiannian@aliyun.com>
2018-09-27 23:53:35 +0800

    sort out format

--- src/project.h
+++ src/project.h
@@ -11,22 +11,22 @@ namespace chest{
 
 class config {
 public:
-	config();
-	config(const filesystem::path &p);
-	config(const config&) = default;
-	~config() = default;
+    config();
+    config(const filesystem::path &p);
+    config(const config&) = default;
+    ~config() = default;
 private:
-	filesystem::path tpath;
-	struct {
-		string name;
-		string family;
-		string version;
-		string maintainer;
-		string upstream;
-		string platform;
-	} project;
-	map<string,string> chest;
-	
+    filesystem::path tpath;
+    struct {
+        string name;
+        string family;
+        string version;
+        string maintainer;
+        string upstream;
+        string platform;
+    } project;
+    map<string,string> chest;
+    
 };
 
 }
