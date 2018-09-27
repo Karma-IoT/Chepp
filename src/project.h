da0bcdd9de3a3ae4030706d095d77728da67675d
tiannian <dtiannian@aliyun.com>
2018-09-28 00:51:10 +0800

    add project config decl

--- src/project.h
+++ src/project.h
@@ -4,6 +4,8 @@
 #include <filesystem>
 #include <string>
 #include <map>
+#include <vector>
+#include <bitset>
 
 using namespace std;
 
@@ -15,18 +17,59 @@ public:
     config(const filesystem::path &p);
     config(const config&) = default;
     ~config() = default;
-private:
-    filesystem::path tpath;
-    struct {
+public:
+    struct project_t {
         string name;
         string family;
         string version;
         string maintainer;
         string upstream;
         string platform;
-    } project;
-    map<string,string> chest;
-    
+    };
+    struct chest_t {
+        string name;
+        bitset type;
+        string upstream;
+        string version;
+        filesystem::path local;
+    };
+    struct rule_t {
+        string command;
+        map<string,string> variable;
+    };
+    struct build_t {
+        rule_t rule;
+        string in;
+        string out;
+        map<string,string> variable;
+    };
+    struct init_t {
+        init_type type;
+        bool require;
+        vector<string> info;
+    };
+public:
+    project_t &project();
+    // some getter
+public:
+    string get_unique_name();
+public:
+    void add_chest(const chest_t &chest);
+    void add_deps(const chest_t &chest);
+public:
+    void do_ninja();
+    void do_init();
+private:
+    filesystem::path tpath;
+    project_t project;
+    vector<chest_t> chest;
+    map<string,rule_t> rule;
+    map<string,build_t> build;
+    map<string,string> variable;
+    map<string,string> command;
+    map<string,init_t> init;
+    map<string,chest_t> deps;
+    ostream &out;
 };
 
 }
